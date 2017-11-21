562b92c866064cb820a186727e5e6b630c914629
Pankayaraj <pankayaraj1995@gmail.com>
2017-11-22 01:12:31 +0530

    11th

--- Multi_arm_bandit_algorithms/GP_bandits/Probability_of_improvement.py
+++ Multi_arm_bandit_algorithms/GP_bandits/Probability_of_improvement.py
@@ -1,15 +1,19 @@
-from scipy.stats import norm
-from scipy.optimize import minimize
+from Machine_learning.Gaussian_process import Gaussian_process
 import tensorflow as tf
 import numpy as np
-from Machine_learning.Gaussian_process import Gaussian_process
+from scipy.optimize import minimize
+from scipy.stats import norm
 
-def Expected_improvement(X, Y, lower_bound, upper_bound, gaussian_mean = 0,
+
+def Probability_of_improvement(X, Y, lower_bound, upper_bound, gaussian_mean = 0,
                          gaussian_variance_weight = 1, gaussian_zigma = 1,
-                         noise = None, expected_imporvement_neta = 0.01,
+                         noise = None, probability_of_improvement_neta = 0.2,
                          datatype = tf.float64, max_iter = 20, return_aqs = False):
 
-    G = Gaussian_process(datatype=datatype)
+    neta = probability_of_improvement_neta
+
+    G = Gaussian_process(datatype)
+
     if noise == None:
         G.fit_noiseless(X, Y, noise=noise, variance_weight=gaussian_variance_weight,
                         mean=gaussian_mean, zigma=gaussian_zigma)
@@ -18,29 +22,24 @@ def Expected_improvement(X, Y, lower_bound, upper_bound, gaussian_mean = 0,
         G.fit_noisy(X, Y, noise= noise, variance_weight=gaussian_variance_weight,
                     mean=gaussian_mean, zigma=gaussian_zigma)
 
+    f_max = max(Y)
 
-    f_max= max(Y)
-    neta = expected_imporvement_neta
 
     def function(x):
         g_ = G.predict(x)
-        g_s = np.sqrt(g_[1])
-        if g_s > 0:
-            Z = (g_[0] - f_max - neta)/g_s
-        else:
-            Z = 0
+        r = (g_[0] - f_max - neta)/np.sqrt(g_[1])
+        ans = 1- norm.cdf(r)
 
-        if g_s == 0:
-            return  0
-        else:
-            ans = ((g_[0] - f_max - neta)*norm.cdf(Z) + g_s*norm.pdf(Z))
-            return ans
+        return -ans
     '''
-    add a - to the return value when using minimise function for maximization
     minima = minimize(function, x0= (lower_bound+upper_bound)/2,
                     bounds=[(lower_bound, upper_bound)],
                     options={'maxiter':max_iter, 'disp':False})['x'][0]
+
+    if return_aqs == False:
+        return minima
     '''
+
     a = np.linspace(lower_bound, upper_bound, 40)
     values = [function(i) for i in a]
 
@@ -51,15 +50,9 @@ def Expected_improvement(X, Y, lower_bound, upper_bound, gaussian_mean = 0,
             minima = a[i]
             minima_value = values[i]
 
-
-
     if return_aqs == False:
-
         return minima
 
     else:
-        x = np.linspace(lower_bound, upper_bound, 40)
-        y = [function(i) for i in x]
-        minima_value = function(minima)
-
-        return minima, y, minima_value
+        return minima, values, minima_value
+    
